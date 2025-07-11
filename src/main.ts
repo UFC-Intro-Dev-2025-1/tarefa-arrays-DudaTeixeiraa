48fca0395ba0d9a62f69c89c4dc8c97c48203319
Eduarda Teixeira <maria.moreira303@aluno.ce.gov.br>
2025-07-11 18:07:14 +0000

    feat: resolucao da atividade main.ts-array.

--- src/main.ts
+++ src/main.ts
@@ -14,31 +14,45 @@
 // 8. Mostrar cada elemento do array em uma linha usando o método forEach. ❌
 // 9. Criar um novo array com os valores dobrados usando o método map. ❌
 // 10. Calcular a soma de todos os elementos do array usando o método reduce. ❌
-
 const numeros: number[] = [10, 20, 30, 40, 50];
 
+// 2. Criar função para exibir array e tamanho
 function mostrarArray(array: number[]): void {
     console.log('--------------------------------');
-    console.log(`Tamanho: `);
-    console.log(`Array: `);
+    console.log(`Tamanho: ${array.length}`);
+    console.log(`Array: ${array}`);
 }
+
+// 3. Adicionar 60 e 70
+numeros.push(60, 70);
+
+// 4. Exibir o array
 mostrarArray(numeros);
 
+// 5. Remover o último elemento
+const elementoExcluido = numeros.pop();
 
-
-
-
-const elementoExcluido = ;
+// 6. Exibir o array novamente
 mostrarArray(numeros);
+
+// 7. Mostrar o elemento excluído
 console.log(`Elemento excluído: ${elementoExcluido}`);
 
-numeros.forEach();
+// 8. Mostrar cada elemento em uma linha com forEach
+numeros.forEach((num) => {
+    console.log(num);
+});
 
-const dobrados = numeros.map();
+// 9. Novo array com valores dobrados
+const dobrados = numeros.map((num) => num * 2);
+
+// Exibir array dobrado
 mostrarArray(dobrados);
 
-const soma = numeros.reduce();
+// 10. Soma dos elementos do array original
+const soma = numeros.reduce((acum, valor) => acum + valor, 0);
 console.log(`Soma dos elementos do array: ${soma}`);
+
 
 // para rodar o código, use o comando:
 // npm run build
